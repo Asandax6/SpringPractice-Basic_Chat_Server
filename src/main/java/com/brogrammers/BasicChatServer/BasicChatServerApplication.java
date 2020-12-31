1a24d2d3c74429198bed023033679817fe614e52
Asandax6 <asandax6@gmail.com>
2020-12-31 20:55:10 +0200

    First Commit

---
+++ src/main/java/com/brogrammers/BasicChatServer/BasicChatServerApplication.java
@@ -0,0 +1,13 @@
+package com.brogrammers.BasicChatServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicChatServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicChatServerApplication.class, args);
+	}
+
+}
